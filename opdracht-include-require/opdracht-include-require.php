f41c7d59b47b553c9fa3d5580a50de34f52d430c
manarkbyeh <manar.kbyeh@student.kdg.be>
2016-11-21 11:46:30 +0100

    Deadline 5

---
+++ opdracht-include-require/opdracht-include-require.php
@@ -0,0 +1,24 @@
+<?php
+
+
+	$artikels = array(
+					array("title" => "Why is airline food so bad?",
+						  "text"  => "So, you just peeled back the plastic off a freshly-delivered tray right off your airplane’s trolley cart and the mess looking back at you is a grim one. The fault may not lie with the chef, though, but in the plane’s design.",
+						  "tags" => array("bbs","airline food"),
+						  ),
+					array("title" => "This is the electric jaguar",
+						  "text"  => "There’s a moment when the future becomes real, when it just kind of slaps you in the face with its inevitable corporeality. That happened — oddly, in virtual reality — last night, when Jaguar announced the I-Pace concept. The presentation was ",
+						  "tags" => array("jaguar"),
+						  ),
+					array("title" => "The story behind chinas minecraft military camo",
+						  "text"  => "Invented by a US Army officer in the 1970s, 'digital camouflage' is pixelating the modern war machine",
+						  "tags" => array("minecraft", "military", "camo"),
+						  ),
+				);
+include 'header-partial.html';
+include 'body-partial.html';
+include 'footer-partial.html';
+
+
+
+ ?>
