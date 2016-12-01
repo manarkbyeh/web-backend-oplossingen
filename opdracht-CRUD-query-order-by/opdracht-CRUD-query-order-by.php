1db042aa29be55bd714f3b0846ab21c7fffef60b
manarkbyeh <manar.kbyeh@student.kdg.be>
2016-12-01 13:21:01 +0100

    deadline 6

--- opdracht-CRUD-query-order-by/opdracht-CRUD-query-order-by.php
+++ opdracht-CRUD-query-order-by/opdracht-CRUD-query-order-by.php
@@ -1,20 +1,20 @@
 <?php
 $messageContainer	=	'';
 $teller=1;
-
+$order_by="";
 try
 {
-    	if (isset($_GET["order_by"]))
-	{
-		$array_query = explode('-',$_GET["order_by"]);
-		$query_order_by = 'my order ' . 	$array_query[0] . ' ' . $array_query[1];
-		
-	}
+    if (isset($_GET["order"]))
+    {
+        $order_query = explode('-',$_GET["order"]);
+        $order_by = "ORDER BY " .    $order_query[0] . " " .    $order_query[1];
+        
+    }
     
     $db = new PDO('mysql:host=localhost;dbname=bieren', 'root', 'root'); // Connectie maken
     $messageContainer	=	'Connectie dmv PDO geslaagd.';
     
-    $sql="SELECT bieren.biernr, bieren.naam, brouwers.brnaam, soorten.soort, bieren.alcohol FROM bieren INNER JOIN brouwers ON bieren.brouwernr = brouwers.brouwernr INNER JOIN soorten ON soorten.soortnr = bieren.soortnr ";
+    $sql="SELECT bieren.biernr, bieren.naam, brouwers.brnaam, soorten.soort, bieren.alcohol FROM bieren INNER JOIN brouwers ON bieren.brouwernr = brouwers.brouwernr INNER JOIN soorten ON soorten.soortnr = bieren.soortnr ".$order_by;
     $statement_o = $db->prepare($sql);
     $statement_o->execute();
     $fetchAssoc = array();
@@ -22,7 +22,7 @@ try
     {
         $fetchAssoc[]	=	$row;
     }
-  
+    
 }
 catch ( PDOException $e )
 {
@@ -36,7 +36,19 @@ catch ( PDOException $e )
   <head>
     <meta charset="utf-8">
     <style>
-
+      .order a {
+        padding-right: 20px;
+        background-repeat: no-repeat;
+        background-position: right;
+      }
+      
+      .ascending a {
+        background-image: url("view_sort_ascending.png");
+      }
+      
+      .descending a {
+        background-image: url("view_sort_descending.png");
+      }
     </style>
   </head>
 
@@ -50,44 +62,53 @@ catch ( PDOException $e )
         <?php echo $messageContainer ?>
       </p>
       <table>
-    <thead>
-      
-      	<?php foreach ($fetchAssoc[0] as $key => $value): ?> 
- 		
- 			
-                         <th class="order <?= ($_GET["order_by"] === $array_query[0] . "-asc") ? "ascending" : "descending"  ?>"><a href="opdracht-CRUD-query-order-by.php?order_by=<?= $key ?><?= ($_GET["order_by"] === $array_query[0] . "-asc")? "-desc" : "-asc" ?>"><?= $key ?></a></th>
+        <thead>
+
+ 
+
+         <?php foreach($fetchAssoc[0] as $key => $value) : ?>
+              <th class="order <?= ($_GET["order"] ===  $order_query[0] . "-asc") ? "ascending" : "descending"  ?>"><a href="opdracht-CRUD-query-order-by.php?order_by=<?= $key ?><?= ($_GET["order"] ===  $order_query[0] . "-asc")? "-desc" : "-asc" ?>"><?= $key ?></a></th>
           <?php endforeach; ?>
-        </tr>
-      </thead>
-      <tbody>
-        <?php foreach($fetchAssoc as $key => $value) : ?>
-          <tr>
-            <?php foreach($value as $k => $v) : ?>
-              <td><?= $v ?></td>
-            <?php endforeach; ?>
-          </tr>
-        <?php endforeach; ?>
-      </tbody>
-    </table>
-  </body>
-</html>
 
-    </thead>
-    <tbody>
-       <?php foreach ($fetchAssoc as $key => $value) : ?>
-        <tr class="<?php echo ($key%2) ? 'even' : 'odd' ?>">    <td><?= $teller++ ?></td>	
-            <?php foreach($value as $row): ?>
-              <td><?= $row ?></td>
-            <?php endforeach?>
+              </tr>
+        </thead>
+        <tbody>
+          <?php foreach($fetchAssoc as $key => $value) : ?>
+            <tr>
+              <?php foreach($value as $k => $v) : ?>
+                <td>
+                  <?= $v ?>
+                </td>
+                <?php endforeach; ?>
             </tr>
-       <?php endforeach;?>
-    </tbody>
-</table>
+            <?php endforeach; ?>
+        </tbody>
+      </table>
+  </body>
+
+  </html>
+
+  </thead>
+  <tbody>
+    <?php foreach ($fetchAssoc as $key => $value) : ?>
+      <tr class="<?php echo ($key%2) ? 'even' : 'odd' ?>">
+        <td>
+          <?= $teller++ ?>
+        </td>
+        <?php foreach($value as $row): ?>
+          <td>
+            <?= $row ?>
+          </td>
+          <?php endforeach?>
+      </tr>
+      <?php endforeach;?>
+  </tbody>
+  </table>
 
 
 
 
-    </section>
+  </section>
 
   </body>
 
