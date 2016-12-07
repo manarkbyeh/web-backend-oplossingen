3ad704701f28d49664ae3c3e8e33ab74a41c3aaf
manarkbyeh <manar.kbyeh@student.kdg.be>
2016-12-07 13:49:34 +0100

    deadline 7

--- opdracht-security-login/registratie-form.php
+++ opdracht-security-login/registratie-form.php
@@ -1,29 +1,41 @@
-<?php 
+<?php
 session_start();
-	if ( isset( $_SESSION[ 'data' ] ) )
-	{
-		$email		=	$_SESSION[ 'data' ][ 'email' ];
-		$password	=	$_SESSION[ 'data' ][ 'password' ];
-	}
+if(isset($_COOKIE['login'])){
+    header("location:dashboard.php");
+}
+
+
 ?>
 
 
 
-<!DOCTYPE html>
-<html>
-<body>
+  <!DOCTYPE html>
+  <html>
 
-<form action="registratie-process.php">
-   e-mail:<br>
-  <input type="e-mail" name="e-mail " >
-  <br>
-  paswoord:<br>
-  <input type="text" name="paswoord" ><input type="submit" value="genereer een paswoord">
-  <br><br>
-  <input type="submit" value="register">
-</form> 
+  <body>
+    <p>
+ <?php if (isset($_SESSION["notification"]))
+{
+    echo $_SESSION["notification"] ;
+}?>
+<form action="registratie-process.php" method="post">
+    <input type="submit" name="session" value="verwijderen">
+</form>
+        <form action="registratie-process.php" method="post">
+          e-mail:
+          <br>
+          <input type="text" name="email" value="<?php if (isset($_SESSION[ 'data' ]["email"])) {echo $_SESSION[ 'data' ]["email"]; }?>">
+          <br> paswoord:
+          <br>
+          <input type="<?php if(isset($_SESSION[ 'data' ]["pasword"])){ echo "text ";} else{echo "password ";}?>" name="password" value="<?php if (isset($_SESSION[ 'data' ]["password"])) { echo $_SESSION[ 'data' ]["password"];}?>">
+          <input type="submit" name="genereer" value="genereer een paswoord">
+          <br>
+          <br>
+          <input type="submit" name="submit" value="register">
+        </form>
 
 
 
-</body>
-</html>
+  </body>
+
+  </html>
--- opdracht-security-login/registratie-process.php
+++ opdracht-security-login/registratie-process.php
@@ -1,10 +1,97 @@
-<?php 
+<?php
 
-
-   function generatePassword( $length = 8 ) {
-    $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-=+;:,.?";
-    $password = substr( str_shuffle( $chars ), 0, $length );
-    return $password;
+session_start();
+$message="";
+$length=8;
+$check=false;
+$email		=	$_SESSION[ 'data' ][ 'email' ];
+$password	=	$_SESSION[ 'data' ][ 'password' ];
+if (isset($_POST['genereer'])) {
+    $_SESSION[ 'data' ][ 'email' ]= $_POST['email'];
+    $_SESSION[ 'data' ][ 'password' ] = generatePassword(8);
+    header("Location: registratie-form.php");
+    exit();
+    
 }
 
+
+if (isset($_POST["submit"]))
+{
+    if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+        $_SESSION["notification"]="";
+        
+        
+        try  {
+            
+            $connect = new PDO('mysql:host=localhost;dbname=opdrachtsecuritylogin', 'root', 'root',array (PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+            $message=	'Connectie dmv PDO geslaagd.';
+        }
+        catch ( PDOException $e )
+        {
+            $message=	'Er ging iets mis: ' . $e->getMessage();
+        }
+    }
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    
+    $sql = "SELECT email FROM users WHERE email = :email";
+    $statement =$connect->prepare($sql);
+    $statement->bindValue(':email', $email);
+    $check=   $statement->execute();
+    $row = $statement->fetch(PDO::FETCH_ASSOC);
+    if($row==0){
+        
+        
+        $sql2 = "INSERT INTO users(email, salt, password, lastlogin)
+        VALUES
+        ( :email, :salt, :password, NOW() )";
+        $statement_u =  $connect->prepare($sql2);
+        $salt = uniqid(mt_rand(), true);
+        $password .= $salt;
+        $email_hash = $email . $salt;
+        $email_hash = hash('sha512', $email_hash);
+        $hash = hash('sha512', $password);
+        $statement_u->bindValue(':email', $email);
+        $statement_u->bindValue(':salt', $salt);
+        $statement_u->bindValue(':password', $hash);
+        $check_u=  $statement_u->execute();
+        if(  $check_u){
+            setcookie('login', $email . ',' . $email_hash, time() + 2592000);
+            
+            header('location:dashboard.php');
+        }	else
+        {
+            $_SESSION["notification"]="er iets fout gegaan";
+            header("Location: registratie-form.php");
+            exit();
+        }
+        
+        
+        
+    }else{
+        $_SESSION["notification"]="deze email bestaat al";
+        header("Location: login.php");
+        exit();
+    }
+    
+}else
+{
+    $_SESSION["notification"]="Vul een email adres aub";
+    header("Location: registratie-form.php");
+    exit();
+}
+
+
+
+
+
+
+function generatePassword($length) {
+    $pull = [];
+    while (count($pull) < $length) {
+        $pull = array_merge($pull, range(0, 9), range('a', 'z'), range('A', 'Z'));
+    }
+    shuffle($pull);
+    return substr(implode($pull), 0, $length);
+}
 ?>
