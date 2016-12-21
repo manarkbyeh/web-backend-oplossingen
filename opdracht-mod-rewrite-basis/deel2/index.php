d7a7d13a8d4ec5501529030416d4687c4c2a466b
manarkbyeh <manar.kbyeh@student.kdg.be>
2016-12-21 22:32:14 +0100

    deadline 9

---
+++ opdracht-mod-rewrite-basis/deel2/index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+    <head>
+        
+        <title>Index</title>
+    </head>
+    <body>
+
+    	<h1>index.php</h1>
+    
+		<h2>var_dump() van $_GET variabele</h2>
+
+		<?php var_dump( $_GET ) ?>
+
+    </body>
+</html>
---
+++ opdracht-mod-rewrite-basis/deel4/original.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Het originele bestand</title>
+</head>
+<body>
+
+
+    
+		<h2>var_dump() van $_GET variabele</h2>
+
+		<?php var_dump( $_GET ) ?>
+
+</body>
+</html>
